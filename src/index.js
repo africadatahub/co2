b84401f49c47db53c558473ed1f5e660d32d6b7e
Di M <meerkdir@gmail.com>
2023-05-15 12:15:13 +0200

    Switched to createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './app.scss';
 
 function App() {
@@ -16,4 +16,6 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<App />);
