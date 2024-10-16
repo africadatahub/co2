481afb8e85aa80f94dbd33ffd18f8ecc189e770e
Di M <meerkdir@gmail.com>
2024-10-16 15:32:03 +0200

    build

---
+++ dist/browser.a127e0d1.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire0ed3.register("fvaAM",(function(e,o){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}));
+//# sourceMappingURL=browser.a127e0d1.js.map
